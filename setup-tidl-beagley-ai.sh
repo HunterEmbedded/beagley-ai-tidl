eafe295aaffcddb513fbf69d9a4a85626fa152c9
Iain Hunter <iain.hunter@r3.group>
2026-05-09 10:25:05 +0100

    add script

---
+++ setup-tidl-beagley-ai.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# script to setup TIDL target on BeagleY-AI running edgeai image
+
+
+git clone https://github.com/TexasInstruments/edgeai-tidl-tools.git || exit
+cd edgeai-tidl-tools || exit
+# match edgeai image version
+git checkout 11_00_08_00
+
+
+# setup for BeagleY-AI
+export SOC=am67a
+export TISDK_IMAGE=edgeai
+export SDK_VERSION=11_0 
+
+
+# now update the target 
+./update_target.sh
+

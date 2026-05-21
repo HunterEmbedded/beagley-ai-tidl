047cf0253f8e5fb3a1f30eec1c9e18b235cc63c8
Iain Hunter <iain@hunterembedded.co.uk>
2026-05-21 13:08:06 +0000

    ensure all setup is done for J722s

--- setup-tidl-beagley-ai.sh
+++ setup-tidl-beagley-ai.sh
@@ -7,17 +7,26 @@
 git config --global user.email "iain@hunterembedded.co.uk"
 git config --global user.name "Iain Hunter"
 
+
+export GIT_SSH_COMMAND='ssh -i /root/.ssh/id_ed25519_github_new.db'
+
+
 git clone https://github.com/TexasInstruments/edgeai-tidl-tools.git || exit
 cd edgeai-tidl-tools || exit
 # match edgeai image version
 git checkout 11_00_08_00 || exit
 git am  ../patches/0001-fix-syntax-error-in-update_target.sh.patch || exit
 
-# setup for BeagleY-AI
-export SOC=am67a
+# setup for BeagleY-AI and make persistent by writing to .profile, overwrite default .profile
+echo " export SOC=am67a 
 export TISDK_IMAGE=edgeai
-export SDK_VERSION=11_0 
+export SDK_VERSION=11_0 " > ~/.profile
 
+# install tools
+./setup.sh
+
+# setup env
+./setup_env.sh am67a
 
 # now update the target 
 ./update_target.sh
